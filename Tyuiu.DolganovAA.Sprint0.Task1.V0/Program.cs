eeb8f8a7f416b6120a92e034e3c5261dcc420831
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2024-09-11 21:25:21 +0300

    Task1

---
+++ Tyuiu.DolganovAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.DolganovAA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello , World");
+            string name = "Александр";
+            int age = 17;
+            Console.WriteLine("Здравствуйте , моё имя " + name + " , и мне " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
