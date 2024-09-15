4a1935628e394879cff1fffe26515c8472a9360f
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2024-09-15 17:50:07 +0300

    Task5

---
+++ Tyuiu.DolganovAA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.DolganovAA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.DolganovAA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B =" + DataService.Addition(1, 5));
+            Console.WriteLine("A - B =" + DataService.Subtraction(98, 5));
+            Console.WriteLine("A * B =" + DataService.Multiplication(13, 5));
+            Console.WriteLine("A / B =" + DataService.Division(10, 5));
+            Console.ReadKey();
+        }
+    }
+}
